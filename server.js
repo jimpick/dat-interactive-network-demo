cbe2732ac420971ddf5cc5032bf536519c806f94
Jim Pick <jim@backblend.com>
2018-07-22 19:40:18 -0700

    Simplify http server ... only runs once

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path')
 var http = require('http')
 
 var server = http.createServer(handler)
-server.listen(process.env.PORT || 5000)
+server.listen(process.env.PORT || 5000, '0.0.0.0')
 server.once('error', function () {
   server.listen(0)
 })
@@ -14,8 +14,18 @@ server.on('listening', function () {
 })
 
 function handler (req, res) {
+  console.log('Jim req url', req.url)
   if (req.url === '/') return file('index.html', 'text/html', res)
   if (req.url === '/bundle.js') return file('bundle.js', 'text/javascript', res)
+  if (req.url === '/events') {
+    // event stream
+    res.setHeader('Content-Type', 'text/event-stream; charset=utf-8')
+    var test = require('./mininet-daemon/replicate-150mb')
+    var attachPath = path.resolve(__dirname, './mininet-daemon/attach')
+    test(attachPath, event => { 
+      res.write('data: ' + JSON.stringify(event) + '\n\n')
+    })
+  }
 }
 
 function file (name, type, res) {
