3cd209da3050bb4bf2bf14f6b2b3afce49a9cadb
Jim Pick <jim@backblend.com>
2018-07-24 22:25:15 -0700

    Renaming and cleanup

--- mininet-daemon/replicate-150mb.js
+++ mininet-daemon/replicate-150mb.js
@@ -1,19 +1,13 @@
-var controller = require('./mnController')
-
-// var {h1, h2} = test.topologies.basic() // 1mbit
+var supervisor = require('./mnSupervisor')
 
 module.exports = run
 
 function run (attachPath, sendTelemetry) {
-  console.log('Jim runTest')
-  var {h1, h2} = controller.basicTopology(2, {bandwidth: 100}) // 100mbit
+  var {h1, h2} = supervisor.basicTopology(2, {bandwidth: 100}) // 100mbit
 
-  controller.start(startNode => {
-  // test('share a dat between two nodes', function (t) {
-    // t.timeoutAfter(25000)
+  supervisor.start(startNode => {
     setTimeout(() => {
-      controller.mininet.on('message', (name, data) => {
-        // console.log('Jim mininet message', name, data)
+      supervisor.mininet.on('message', (name, data) => {
         if (name === 'h1:emit' && data[0] === 'telemetry') {
           sendTelemetry(data[1])
         }
@@ -24,41 +18,15 @@ function run (attachPath, sendTelemetry) {
       var tempy = require('tempy')
 
       var dir = tempy.directory()
-      // t.pass('h1 run')
 
       h2.on('sharing', ({key}) => {
-        // t.pass('h1 received sharing')
         Dat(dir, {key: key, temp: true}, function (err, dat) {
           if (err) throw err
-          // dat.joinNetwork()
-
-          /*
-          var network = dat.joinNetwork(function (err) {
-            t.error(err && err.toString(), 'h1 joinNetwork calls back okay')
-          })
-          network.once('connection', function () {
-            t.pass('h1 got connection')
-          })
-          */
-
-          // t.pass('h1 downloading dat://' + key)
 
           var archive = dat.archive
           require(attachPath)(archive, 3, (message, args) => {
-            // console.log('Jim send message', message, args)
             h1.emit(message, args)
           })
-          if (archive.content) contentReady()
-          archive.once('content', contentReady)
-
-          function contentReady () {
-            // t.pass('h1 content ready')
-            archive.content.on('sync', function () {
-              // t.pass('h1 dat synced')
-              // TODO tests
-              // t.end()
-            })
-          }
         })
       })
     }
@@ -72,19 +40,12 @@ function run (attachPath, sendTelemetry) {
       var Dat = require('dat-node')
       var path = require('path')
       var fixture = path.join(__dirname, '../fixtures/dat2-150mb')
-      // t.pass('h2 run')
       Dat(fixture, {temp: true}, function (err, dat) {
         if (err) throw err
         dat.importFiles()
 
-        var network = dat.joinNetwork(function (err) {
-          // t.error(err && err.toString(), 'h2 joinNetwork calls back okay')
-        })
-        network.once('connection', function () {
-          // t.pass('h2 got connection')
-        })
+        var network = dat.joinNetwork()
 
-        // t.pass('h2 sharing dat://' + dat.key.toString('hex'))
         h2.emit('sharing', {key: dat.key.toString('hex')})
       })
     })
